31933580758c4446abc37a876e9724e53ed06b81
Stephen Krewson <stephen.krewson@gmail.com>
2016-07-07 15:55:10 -0400

    stuff with arrays

--- TestPrint.java
+++ TestPrint.java
@@ -1,10 +1,26 @@
+import java.util.Arrays;
+
 public class TestPrint
 {
 	public static void main ( String[] args)
 	{
 		System.out.println( "MAX " + Integer.MAX_VALUE + "MIN " + Integer.MIN_VALUE);
 
+		// Some Array examples
 		int[] myArray = {1, 2, 3, 4, 5, 6};
+
+		System.out.println(Arrays.toString(myArray));
+		
+		// Setting an array with bounds
+		// need "new" keyword (reserved) since we aren't explicitly
+		// declaring and filling up the object, as with myArray.
+		final int MAX_ARRAY = 30;
+		int[] bigArray = new int[MAX_ARRAY];
+
+		bigArray[29] = 67;
+
+		System.out.println(Arrays.toString(bigArray));
+
 
 		for (int elem : myArray) {
 			System.out.println(elem);
